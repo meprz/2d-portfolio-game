cbb519943f1265d1d6894588add7473cf1c9e2a4
Miguel Perez <migeperezjr@gmail.com>
2024-04-09 01:51:58 -0500

    added placeholder text for the objects on the map

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,13 @@
 export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `Add dialogue here at the PC.`,
+    "cs-degree": `Add dialogue here at the DEGREE.`,
+    "sofa-table": `Add dialogue here at the SOFA.`,
+    tv: `Add dialogue here at the TV.`,
+    bed: `Add dialogue here at the BED.`,
+    resume: `Add dialogue here at the RESUME.`,
+    projects: `Add dialogue here at the PROJECTS.`,
+    library: `Add dialogue here at the LIBRARY.`,
+    exit: `Add dialogue here at the EXIT.`,
+};
